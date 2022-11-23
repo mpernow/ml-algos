ad9d6eee0a3bca82c1b1c503a1a4150f41c12083
Marcus Pernow <marcus.pernow@gmail.com>
2022-11-23 22:26:54 +0100

    simple setup script and requirements

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from os import path
+
+__version__ = '0.0.1'
+
+root_dir = path.abspath(path.dirname(__file__))
+
+# get the dependencies and installs
+with open(path.join(root_dir, 'requirements.txt'), 'r') as f:
+    requirements = f.read().split('\n')
+
+setup(
+    name='ml-algos',
+    version=__version__,
+    description='Implementation of some machine learning algorithms.',
+    url='https://github.com/mpernow/ml-algos',
+    download_url='https://github.com/mpernow/ml-algos/tarball/master',
+    license='MIT',
+    author='Marcus Pernow',
+    author_email='marcus.pernow@gmail.com',
+    packages=find_packages(),
+    install_requires=requirements
+)
