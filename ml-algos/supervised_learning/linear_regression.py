6b74de7e90ffe384352b9ae346772890541a9551
Marcus Pernow <marcus.pernow@gmail.com>
2022-11-27 16:45:52 +0100

    comments added

--- ml-algos/supervised_learning/linear_regression.py
+++ ml-algos/supervised_learning/linear_regression.py
@@ -60,15 +60,20 @@ class RidgeRegression:
     ):
         """
         Computes the optimal parameters beta_hat for the given training data using ridge regression.
+        Note that the input ought to be standardised, since ridge regression is not invariant under scaling.
 
         Args:
             X   (np.array): Training data of shape (n_samples, n_features)
             y   (np.array): Target values for the training data with shape (n_samples)
         """
+        # Create a penalty term
         p = X.shape[1] + 1
         penalty_term = self.lam * np.eye(p)
+        # Set the (0,0) element to zero so as not to penalise intercept parameter
         penalty_term[0][0] = 0
+        # Add a feature row of ones for the intercept
         X_with_bias = np.insert(X, 0, 1., axis=1)
+        # Compute the coefficients
         self.beta_hat = spl.inv(X_with_bias.T @ X_with_bias + penalty_term) @ X_with_bias.T @ y
     
     def predict(
