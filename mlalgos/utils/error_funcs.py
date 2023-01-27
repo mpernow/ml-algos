db9e980df7aa84ca0a0267d2193476193eac6625
Marcus Pernow <marcus.pernow@gmail.com>
2023-01-27 07:52:22 +0100

    misclassification error

--- mlalgos/utils/error_funcs.py
+++ mlalgos/utils/error_funcs.py
@@ -16,3 +16,20 @@ def mean_squared_error(
         float: The computed mean squared error
     """
     return np.mean((y_true - y_pred) ** 2)
+
+def misclassification_error(
+    y_true: np.array,
+    y_pred: np.array
+) -> float:
+    """
+    Computes the misclassification error between the true and predicted values.
+
+    Args:
+        y_true (np.array): The true values as a numpy array of shape (n_pred)
+        y_pred (np.array): The predicted values as a numpy array of shape (n_pred)
+
+    Returns:
+        float: The fraction of missclassified values
+    """
+    N = len(y_true)
+    return (1./N) * np.sum(np.array(y_true) != np.array(y_pred))
