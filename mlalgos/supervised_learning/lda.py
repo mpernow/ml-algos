4e5a9eba25702d379a94ad3582f0f1a8df9a4bf1
Marcus Pernow <marcus.pernow@gmail.com>
2023-02-21 09:13:44 +0100

    initial LDA commit

---
+++ mlalgos/supervised_learning/lda.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class LDA:
+    def __init__(
+        self
+    ):
+        """
+        Initialises a linear discriminant analysis classifier.
+        """
+        pass
+    
+    def fit(
+        X: np.array,
+        y: np.array
+    ):
+        """
+        Fits the LDA classifier.
+
+        Args:
+            X (np.array): The features as an array of shape (n_samples, n_features)
+            y (np.array): The targets as an array of shape (n_samples)
+        """
+        pass
