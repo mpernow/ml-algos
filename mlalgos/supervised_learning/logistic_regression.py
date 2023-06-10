1d09b01ad0845eaea012a7f5333ddc2c01deabd7
Marcus Pernow <marcus.pernow@gmail.com>
2023-06-10 17:45:30 +0200

    initial commit

---
+++ mlalgos/supervised_learning/logistic_regression.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class LogisticRegression:
+    def __init__(
+            self
+    ):
+        """
+        Initialises a logistic regression classifier.
+        """
+        pass
